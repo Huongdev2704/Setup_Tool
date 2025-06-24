b7edc527d3c357fc0abd4ea4fd824ac50ed66342
Huongdev2704 <huongdev2704@gmail.com>
2025-06-24 17:26:18 +0700

    Update setup.sh
    sd

--- setup.sh
+++ setup.sh
@@ -5,13 +5,21 @@ pkg update -y && pkg upgrade -y
 pkg install git -y
 pkg install proot-distro -y
 
+# Clone tool proot-distro (nếu cần)
 git clone https://github.com/termux/proot-distro
 cd proot-distro
 ./install.sh
 
+# Cài Ubuntu (LTS)
 proot-distro install ubuntu
 
+# Cài Python 3.11 trong Ubuntu
 proot-distro login ubuntu -- bash -c "
 apt update -y
-apt install python3 python3-pip -y
-echo '✅ Cài xong tất cả thư viện!'"
+apt install software-properties-common -y
+add-apt-repository ppa:deadsnakes/ppa -y
+apt update -y
+apt install python3.11 python3.11-venv python3.11-dev -y
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 1
+python3 --version
+echo '✅ Đã cài Python 3.11.0 xong!'"
