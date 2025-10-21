bc3507aabcd5e1673d880874a53ab86ac8f695dd
Huongdev2704 <huongdev2704@gmail.com>
2025-10-21 11:18:08 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,21 +1,18 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 pkg update -y && pkg upgrade -y
-pkg install proot-distro wget -y
-
+pkg install wget tar proot-distro -y
 proot-distro remove ubuntu 2>/dev/null || true
 
 mkdir -p ~/ubuntu-rootfs
 cd ~/ubuntu-rootfs
 wget https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-arm64-root.tar.xz
-tar -xJf noble-server-cloudimg-arm64-root.tar.xz -C ~/
-
-# Giả sử giải nén ra thư mục gốc ở ~/, import
-proot-distro import ubuntu-24.04 ~/
+tar -xJf noble-server-cloudimg-arm64-root.tar.xz -C ~/ubuntu-rootfs
+proot-distro import ubuntu-24.04 ~/ubuntu-rootfs
 
 proot-distro login ubuntu-24.04 -- bash -c "
 apt update -y
-apt install python3 python3-pip -y
+apt install -y python3 python3-pip
 python3 --version
-echo '✅ Ubuntu 24.04 LTS + Python 3.12.3!'
+echo '✅ Installed Ubuntu 24.04 LTS with Python 3.12.3!'
 "
