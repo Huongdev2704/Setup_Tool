d36cc09e3276c8302f2380c3c6148f4de0a2edc3
Huongdev2704 <huongdev2704@gmail.com>
2025-10-21 09:38:36 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/data/data/com.termux/files/usr/bin/bash
+Lênh này là nó cài bản python mới nhất à #!/data/data/com.termux/files/usr/bin/bash
 
 termux-setup-storage
 pkg update -y && pkg upgrade -y
@@ -13,16 +13,5 @@ proot-distro install ubuntu
 
 proot-distro login ubuntu -- bash -c "
 apt update -y
-apt install software-properties-common -y
-add-apt-repository ppa:deadsnakes/ppa -y
-apt update -y
-apt install python3.12 python3.12-venv python3.12-dev python3-pip -y
-
-# Set python3 mặc định là 3.12
-update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.12 2
-
-echo ''
-python3 --version
-pip3 --version
-echo '✅ Cài Python 3.12 thành công!'
-"
+apt install python3 python3-pip -y
+echo '✅ Cài xong tất cả thư viện!'"
