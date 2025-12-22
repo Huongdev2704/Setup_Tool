ca3d21d5d142f91d0d308af38f00791188de3174
Huongdev2704 <huongdev2704@gmail.com>
2025-12-22 13:38:33 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,29 +1,17 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 termux-setup-storage
-
 pkg update -y && pkg upgrade -y
-pkg install git proot-distro -y
+pkg install git -y
+pkg install proot-distro -y
+
+git clone https://github.com/termux/proot-distro
+cd proot-distro
+./install.sh
 
 proot-distro install ubuntu
 
 proot-distro login ubuntu -- bash -c "
 apt update -y
-
-# Cài Python 3.11 + pip + venv + build tools
-apt install -y software-properties-common
-apt install -y python3.11 python3.11-venv python3.11-dev python3-pip \
-               build-essential curl wget git
-
-# Set python3 = python3.11
-update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 1
-
-# Tạo venv
-python3 -m venv venv
-source venv/bin/activate
-
-# Update pip tools
-python -m pip install --upgrade pip setuptools wheel
-
-echo '✅ Python 3.11 + venv + pip OK'
-"
+apt install python3 python3-pip -y
+echo '✅ Cài xong tất cả thư viện!'"
